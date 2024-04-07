0b5c2050e2af7a54346e0ef9be2825eeb58a2330
Rymjbeli <rim.jbeli@insat.ucar.tn>
2024-04-07 14:19:54 +0100

    First commit with some middlewares

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
+dotenv.config();
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'process.env.DB_HOST',
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: 'process.env.DB_USERNAME',
+    //   password: 'process.env.DB_PASSWORD',
+    //   database: 'process.env.DB_NAME',
+    //   autoLoadEntities: true,
+    //   entities: ['dist/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    //   debug: false,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HelmetMiddleware).forRoutes('');
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useStaticAssets(path.join(__dirname, '../uploads'));
+  await app.listen(5000);
+}
+bootstrap();
