df970e888b40ff64eb35d5a0cc383cf7e4f91496
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-04-20 23:10:59 +0100

    announcements crud

--- src/announcement/announcement.module.ts
+++ src/announcement/announcement.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AnnouncementService } from './announcement.service';
 import { AnnouncementController } from './announcement.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnnouncementEntity } from './entities/announcement.entity';
+import { SubjectService } from 'src/subject/subject.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([AnnouncementEntity]), 
+  ],
   controllers: [AnnouncementController],
-  providers: [AnnouncementService],
+  providers: [AnnouncementService,SubjectService,UserService],
 })
 export class AnnouncementModule {}
