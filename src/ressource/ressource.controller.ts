4c912427048311f2bd5f4e99a496c2ac763229f4
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-05-05 14:12:48 +0100

    resouces (without upload

--- src/ressource/ressource.controller.ts
+++ src/ressource/ressource.controller.ts
@@ -6,18 +6,30 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { RessourceService } from './ressource.service';
 import { CreateRessourceDto } from './dto/create-ressource.dto';
 import { UpdateRessourceDto } from './dto/update-ressource.dto';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('ressource')
 export class RessourceController {
   constructor(private readonly ressourceService: RessourceService) {}
 
   @Post()
-  create(@Body() createRessourceDto: CreateRessourceDto) {
-    return this.ressourceService.create(createRessourceDto);
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  create( @Body() createRessourceDto: CreateRessourceDto,     
+          @CurrentUser()user:User,
+          @UploadedFile() file: Express.Multer.File
+         ){
+    return this.ressourceService.create(createRessourceDto, user, file);
   }
 
   @Get()
@@ -25,21 +37,13 @@ export class RessourceController {
     return this.ressourceService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ressourceService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateRessourceDto: UpdateRessourceDto,
-  ) {
-    return this.ressourceService.update(+id, updateRessourceDto);
-  }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ressourceService.remove(+id);
+  }
+
+  @Get('recover/:id')
+  recover(@Param('id') id: string) {
+    return this.ressourceService.recover(+id);
   }
 }
