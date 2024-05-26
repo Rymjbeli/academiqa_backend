b3f875dfd69ff88f5b34a2f1892a917fbfe6c31e
saradrine <sarra.drine@insat.ucar.tn>
2024-05-26 20:48:54 +0100

    dto

--- src/note/dto/update-note.dto.ts
+++ src/note/dto/update-note.dto.ts
@@ -1,7 +1,5 @@
 import { IsOptional, IsString } from 'class-validator';
-import { Exclude } from 'class-transformer';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateNoteDto } from './create-note.dto';
 
-@Exclude()
 export class UpdateNoteDto extends PartialType(CreateNoteDto) {}
--- src/session/dto/get-session.dto.ts
+++ src/session/dto/get-session.dto.ts
@@ -1,30 +1,22 @@
 import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { SessionTypeEntity } from '../../session-type/entities/session-type.entity';
-import { Exclude, Expose } from 'class-transformer';
 import { SessionTypeEnum } from '../../Enums/session-type.enum';
 
-@Exclude()
 export class GetSessionDto {
-  @Expose()
   @IsNumber()
   id: number;
 
-  @Expose()
   @IsNotEmpty()
   StartTime: Date;
 
-  @Expose()
   @IsNotEmpty()
   EndTime: Date;
 
-  @Expose()
   holidayName?: string[];
 
-  @Expose()
   @IsString()
   name?: string;
 
-  @Expose()
   @IsNotEmpty()
   @IsEnum(SessionTypeEnum)
   type: SessionTypeEnum;
